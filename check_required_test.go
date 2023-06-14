718e2d950e9bb53c3706e2841db83a215f310b6e
Onur Cinar <onur.cinar@gmail.com>
2023-06-13 20:35:05 -0700

    Check required is added.

---
+++ check_required_test.go
@@ -0,0 +1,104 @@
+package checker
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCheckRequiredValidString(t *testing.T) {
+	s := "valid"
+
+	if checkRequired(reflect.ValueOf(s)) != ResultValid {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredUninitializedString(t *testing.T) {
+	var s string
+
+	if checkRequired(reflect.ValueOf(s)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredEmptyString(t *testing.T) {
+	s := ""
+
+	if checkRequired(reflect.ValueOf(s)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredUninitializedNumber(t *testing.T) {
+	var n int
+
+	if checkRequired(reflect.ValueOf(n)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredValidSlice(t *testing.T) {
+	s := []int{1}
+
+	if checkRequired(reflect.ValueOf(s)) != ResultValid {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredUninitializedSlice(t *testing.T) {
+	var s []int
+
+	if checkRequired(reflect.ValueOf(s)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredEmptySlice(t *testing.T) {
+	s := make([]int, 0)
+
+	if checkRequired(reflect.ValueOf(s)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredValidArray(t *testing.T) {
+	s := [1]int{1}
+
+	if checkRequired(reflect.ValueOf(s)) != ResultValid {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredEmptyArray(t *testing.T) {
+	s := [1]int{}
+
+	if checkRequired(reflect.ValueOf(s)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredValidMap(t *testing.T) {
+	m := map[string]string{
+		"a": "b",
+	}
+
+	if checkRequired(reflect.ValueOf(m)) != ResultValid {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredUninitializedMap(t *testing.T) {
+	var m map[string]string
+
+	if checkRequired(reflect.ValueOf(m)) != ResultRequired {
+		t.Fail()
+	}
+}
+
+func TestCheckRequiredEmptyMap(t *testing.T) {
+	m := map[string]string{}
+
+	if checkRequired(reflect.ValueOf(m)) != ResultRequired {
+		t.Fail()
+	}
+}
--- checker.go
+++ checker.go
@@ -1,2 +1,14 @@
 // Package checker is a Go library for validating user input through struct tags.
 package checker
+
+// Result is a unique textual identifier for the mistake.
+type Result string
+
+// ResultValid result indicates that the user input is valid.
+const ResultValid Result = "VALID"
+
+// Mistake provides the field where the mistake was made and a result for the mistake.
+type Mistake struct {
+	Field  string
+	Result Result
+}
