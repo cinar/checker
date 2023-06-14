eb2734e4807e58dce9d48c05689649b500aaaaae
Onur Cinar <onur.cinar@gmail.com>
2023-06-13 20:43:56 -0700

    Maker function.

--- checker.go
+++ checker.go
@@ -1,14 +1,27 @@
 // Package checker is a Go library for validating user input through struct tags.
 package checker
 
+import "reflect"
+
 // Result is a unique textual identifier for the mistake.
 type Result string
 
-// ResultValid result indicates that the user input is valid.
-const ResultValid Result = "VALID"
+// CheckerFunc defines the checker function.
+type CheckerFunc func(value, parent reflect.Value) Result
+
+// MakerFunc defines the maker function.
+type MakerFunc func(config string) CheckerFunc
 
 // Mistake provides the field where the mistake was made and a result for the mistake.
 type Mistake struct {
 	Field  string
 	Result Result
+}
+
+// ResultValid result indicates that the user input is valid.
+const ResultValid Result = "VALID"
+
+// Register registers the given checker name and the maker function.
+func Register(name string, maker MakerFunc) {
+
 }
