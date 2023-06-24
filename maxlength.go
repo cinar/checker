60c7d696d93e03ae043bdcb9c3b0f4e69376b03c
Onur Cinar <onur.cinar@gmail.com>
2023-06-24 19:07:28 +0000

    Added copyright header to the code. Fixes #112

--- maxlength.go
+++ maxlength.go
@@ -1,3 +1,11 @@
+// Package checker is a Go library for validating user input through struct tags.
+//
+// https://github.com/cinar/checker
+//
+// Copyright 2023 Onur Cinar. All rights reserved. 
+// Use of this source code is governed by a MIT-style 
+// license that can be found in the LICENSE file. 
+//
 package checker
 
 import (
