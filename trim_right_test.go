e17f653a8026424afba7fd62a04c8eed393ce2e0
Onur Cinar <onur.cinar@gmail.com>
2023-06-24 18:49:37 +0000

    Add copyright to the source code. Fixes #112

--- trim_right_test.go
+++ trim_right_test.go
@@ -1,3 +1,9 @@
+// Package checker is a Go library for validating user input through struct tags.
+//
+// Copyright (c) 2023 Onur Cinar. All Rights Reserved.
+// The source code is provided under MIT License.
+//
+// https://github.com/cinar/checker
 package checker_test
 
 import (
