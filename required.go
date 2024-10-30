04635fcca1c34a0d39a17f2a38f8997086b3af9a
Onur Cinar <onur.cinar@gmail.com>
2024-10-29 19:43:20 -0700

    Copyright message is added to all source files. (#123)
    # Describe Request
    
    Copyright message is added to all source files.
    
    # Change Type
    
    Code maintenance.

--- required.go
+++ required.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2023-2024 Onur Cinar.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+// https://github.com/cinar/checker
+
 package checker
 
 import "reflect"
